96688d728c5202b3ac2f04943377b0b57cdbd72c
Sam Delay <samuel.delay@gmail.com>
2019-12-26 17:55:40 -0500

    created nav, about me, added pics

---
+++ style/script.js
@@ -0,0 +1,8 @@
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('.sidenav');
+//     var instances = M.Sidenav.init(elems, options);
+//   });
+
+const hamMenu = document.querySelectorAll(".sidenav");
+       M.Sidenav.init(hamMenu,{});
